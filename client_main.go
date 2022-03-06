8f84dc9c820da2b5798704899fbfe836fbfe65e9
PudgeKim <zxaq15@gmail.com>
2022-03-06 13:54:31 +0900

    Add pb files

---
+++ client_main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	pb "github.com/Pudgekim/protos"
+	"google.golang.org/grpc"
+	"time"
+)
+
+const address = "localhost:6060"
+
+func main() {
+	clientMain()
+}
+
+func clientMain() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		fmt.Println("client did not connect: ", err.Error())
+	}
+	defer conn.Close()
+
+	client := pb.NewAuthClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	user, err := client.GetUser(ctx, &pb.UserId{Id: "107848443863300905065"})
+	if err != nil {
+		fmt.Println("client_main_user: ", user)
+	}
+	fmt.Println("client_main_user: ", user)
+}
